f8f8a2e54a9292c7230b36b361fc079081d6e0e4
Mizutani Lab <mizutanilaboratory@gmail.com>
2020-09-24 06:00:30 +0900

    Update MNIST_3layersSchizo200905.py

--- paperfigs/MNIST_3layersSchizo200905.py
+++ paperfigs/MNIST_3layersSchizo200905.py
@@ -58,7 +58,7 @@ for id in idlist:
       print('id=', id, 'irepeat=', irepeat, '/', num_repeat)
     model = keras.Sequential([
       layers.Flatten(),
-      schizo.SzDense(idim, reduction_ratio=id, form='diagonal', activation='relu', kernel_initializer='he_normal'),
+      schizo.SzDense(idim, param_reduction=id, form='diagonal', activation='relu', kernel_initializer='he_normal'),
       layers.Dense(num_class, activation='softmax')
     ])
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
